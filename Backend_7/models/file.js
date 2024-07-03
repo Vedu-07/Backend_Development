ae161f7ee6d608aad0269b5e399a12174dc08574
Vedu-07 <vedantswami02@gmail.com>
2024-07-03 13:40:45 +0530

    Email Functionality Added Using Nodemailer

--- Backend_7/models/file.js
+++ Backend_7/models/file.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -16,6 +17,32 @@ const fileSchema = new mongoose.Schema({
     }
 })
 
+// Post Middleware
+fileSchema.post("save",async function(doc){
+    try{
+        console.log("Doc ", doc);
+
+        // Transporter
+        const transporter = nodemailer.createTransport({
+            host:process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            }
+        })
+
+        // Send Mail
+        let info = await transporter.sendMail({
+            from: `Kingdom`,
+            to:doc.email,
+            subject:"New File Uploaded On CLoudinary",
+            html:`<h2> Hello Jiii File Uploaded Braahhhh!!!!! View Here: <a href="${doc.imageUrl}>${doc.imageUrl}</a></h2>"`
+        })
+    }
+    catch (error){
+        console.error(error);
+    }
+})
 
 const File = mongoose.model("File",fileSchema);
 module.exports = File;
